727d67de1a0a35f7b3f7eb93a78fe1afe70d9732
Keshav <keshavchandak2408@gmail.com>
2023-01-05 22:21:52 +0530

    Commit

---
+++ ecomm v1.1/demo/Seller_Keshav/Keshav_Seller.java
@@ -0,0 +1,115 @@
+package demo.Seller_Keshav;
+import java.util.ArrayList;
+import java.util.Random;
+
+import ecomm.Globals;
+import ecomm.Platform;
+import ecomm.Product;
+import ecomm.Seller;
+
+public class Keshav_Seller extends Seller{
+    private ArrayList<ProductMain> productList=new ArrayList<ProductMain>();
+    private static int p_id=0;
+    public Keshav_Seller(String id) {
+        super(id);
+        addBook();
+        addMobile();
+    }
+
+    public void addPlatform(Platform thePlatform) {
+        thePlatform.addSeller(this);//we add the seller to the platform
+    }
+
+    
+    public String generateBookName() {
+        String[] book_names={"Introduction_To-Algorithm","Digital_Design","Machine_Learning","Maths_For_Machine_Learning","Topology","Number_Theory","Statistical_Physics","Astronomy","Computer_Architecture","Computer_Networks"};
+        Random random = new Random();
+        int index = random.nextInt(book_names.length);
+        return getID()+"-"+book_names[index];
+    }
+
+    public String generateProductID() {
+        return getID()+"-"+Integer.toString(100+(p_id++));
+    }
+
+    public float generatePrice() {
+        Random rd = new Random(); // creating Random object
+        return rd.nextFloat((float) 1000); // displaying a random float value between 0.0 and 1.0
+    }
+
+    public int generateQuantity() {
+        Random rd = new Random(); // creating Random object
+        return rd.nextInt(200);
+    }
+
+	public void addBook()
+    {
+        int no_of_books=10;
+        while(no_of_books-->0)
+        {
+            String book_name=generateBookName();
+            String product_id=generateProductID();
+            float book_price=generatePrice();
+            int quantity=generateQuantity(); 
+            Book b = new Book(Globals.Category.Book, book_name, product_id, book_price, quantity);
+            productList.add(b);
+        }
+    }
+    
+    public void addMobile()
+    {
+        int no_of_mobile=10;
+        while(no_of_mobile-->0)
+        {
+            String mobile_name=generateMobileName();
+            String product_id=generateProductID();
+            float mobile_price=generatePrice();
+            int quantity=generateQuantity(); 
+            Mobile m=new Mobile(Globals.Category.Mobile, mobile_name, product_id, mobile_price, quantity);
+            productList.add(m);
+        }
+    }
+
+	public String generateMobileName() {
+        String[] mobile_names={"Samsung","Apple","Blackberry","Nokia","RedMi","RealMe","ASUS"};
+        Random random = new Random();
+        int index = random.nextInt(mobile_names.length);
+        return mobile_names[index];
+    }
+
+    // Seller to return listing of Products of specified Category
+	public ArrayList<Product> findProducts(Globals.Category whichOne)
+    {
+        ArrayList<Product> listing = new ArrayList<Product>();
+        for(ProductMain i: productList)
+        {
+            if(i.getCategory().equals(whichOne))
+            {
+                listing.add(i);
+            }
+        }
+        return listing;
+    }
+
+	// User wants to buy specified quantity of productID
+	// Return true if transaction succeeds, false otherwise. 
+	// Transaction fails if incorrect productID or quantity exceeds available inventory
+	public boolean buyProduct(String productID, int quantity)
+    {
+        for(ProductMain i:productList)
+        {
+            if(i.getProductID().equals(productID))
+            {
+                if(i.getQuantity()>=quantity)
+                {
+                    i.reduceQuantity(quantity);
+                    i.increasePrice();
+                    return true;
+                }
+                else
+                    return false;
+            }
+        }
+        return false;
+    }
+}
--- ecomm v1.1/demo/Seller_Ricky/Ricky_Seller.java
+++ ecomm v1.1/demo/Seller_Ricky/Ricky_Seller.java
@@ -1,4 +1,4 @@
-package demo.Seller_Rishi;
+package demo.Seller_Ricky;
 import java.util.ArrayList;
 
 import ecomm.Globals;
@@ -6,9 +6,9 @@ import ecomm.Platform;
 import ecomm.Product;
 import ecomm.Seller;
 
-public class Seller1 extends Seller{
+public class Ricky_Seller extends Seller{
     private ArrayList<ProductMain> productList=new ArrayList<ProductMain>();
-    public Seller1(String id) {
+    public Ricky_Seller(String id) {
         super(id);
         addBook();
         addMobile();
@@ -17,15 +17,14 @@ public class Seller1 extends Seller{
     {
         thePlatform.addSeller(this);//we add the seller to the platform
     }
-
 	public void addBook()
     {
-        Book b1=new Book(Globals.Category.Book,getID()+"-"+"Statistics",getID()+"1", (float) 1000.0, 360);
-        Book b2=new Book(Globals.Category.Book,getID()+"-"+"Probability",getID()+"2", (float) 1250.0, 180);
-        Book b3=new Book(Globals.Category.Book,getID()+"-"+"Number Theory",getID()+"3", (float) 1300.0, 120);
-        Book b4=new Book(Globals.Category.Book,getID()+"-"+"Regression",getID()+"4", (float) 1880.0, 100);
-        Book b5=new Book(Globals.Category.Book,getID()+"-"+"Topology",getID()+"5", (float) 590.0, 80);
-        Book b6=new Book(Globals.Category.Book,getID()+"-"+"Graph Theory",getID()+"6", (float) 260.0,65);
+        Book b1=new Book(Globals.Category.Book,getID()+"-"+"Hamlet",getID()+"100", (float) 400.0, 90);
+        Book b2=new Book(Globals.Category.Book,getID()+"-"+"The Merchant Of Venice",getID()+"101", (float) 250.0, 220);
+        Book b3=new Book(Globals.Category.Book,getID()+"-"+"The Isles of Naath",getID()+"102", (float) 100.0, 10);
+        Book b4=new Book(Globals.Category.Book,getID()+"-"+"The Song Of Ice And Fire",getID()+"103", (float) 200.0, 140);
+        Book b5=new Book(Globals.Category.Book,getID()+"-"+"Macbeth",getID()+"104", (float) 890.0, 20);
+        Book b6=new Book(Globals.Category.Book,getID()+"-"+"Chernobyl",getID()+"105", (float) 360.0,85);
         productList.add(b1);
         productList.add(b2);
         productList.add(b3);
@@ -36,12 +35,12 @@ public class Seller1 extends Seller{
     
     public void addMobile()
     {
-        Mobile b1=new Mobile(Globals.Category.Mobile,getID()+"-"+"HP",getID()+"500", (float) 3200.0, 110);
-        Mobile b2=new Mobile(Globals.Category.Mobile,getID()+"-"+"Samsung",getID()+"501", (float) 2000.0, 80);
-        Mobile b3=new Mobile(Globals.Category.Mobile,getID()+"-"+"Apple",getID()+"502", (float) 1590.0, 50);
-        Mobile b4=new Mobile(Globals.Category.Mobile,getID()+"-"+"Blackberry",getID()+"503", (float) 64200.0, 30);
-        Mobile b5=new Mobile(Globals.Category.Mobile,getID()+"-"+"Asus",getID()+"504", (float) 8420.0, 180);
-        Mobile b6=new Mobile(Globals.Category.Mobile,getID()+"-"+"RealMe",getID()+"505", (float) 7740.0,185);
+        Mobile b1=new Mobile(Globals.Category.Mobile,getID()+"-"+"HP Notebook",getID()+"500", (float) 40000.0, 30);
+        Mobile b2=new Mobile(Globals.Category.Mobile,getID()+"-"+"ASUS Zen Laptop",getID()+"501", (float) 25000.0, 20);
+        Mobile b3=new Mobile(Globals.Category.Mobile,getID()+"-"+"Apple M1 Pro",getID()+"502", (float) 15590.0, 10);
+        Mobile b4=new Mobile(Globals.Category.Mobile,getID()+"-"+"Redmi Notebook Pro",getID()+"503", (float) 34200.0, 40);
+        Mobile b5=new Mobile(Globals.Category.Mobile,getID()+"-"+"Infinix Pro Inspiron",getID()+"504", (float) 94720.0, 20);
+        Mobile b6=new Mobile(Globals.Category.Mobile,getID()+"-"+"RealMe Desktop",getID()+"505", (float) 17740.0,85);
         productList.add(b1);
         productList.add(b2);
         productList.add(b3);
@@ -58,7 +57,6 @@ public class Seller1 extends Seller{
             if(i.getCategory().equals(whichOne))
             {
                 listing.add(i);
-                i.increasePrice();
             }
         }
         return listing;
@@ -75,6 +73,7 @@ public class Seller1 extends Seller{
                 if(i.getQuantity()>=quantity)
                 {
                     i.reduceQuantity(quantity);
+                    i.changePriceRandomly();
                     return true;
                 }
                 else
