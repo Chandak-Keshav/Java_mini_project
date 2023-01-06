12339f89a8e5b60f24c58015f57a25c4abed6659
Rishi0002 <92517544+Rishi0002@users.noreply.github.com>
2023-01-06 22:20:56 +0530

    Update Mobile.java

--- ecomm v1.1/demo/Seller_Rishi/Mobile.java
+++ ecomm v1.1/demo/Seller_Rishi/Mobile.java
@@ -3,8 +3,12 @@ package demo.Seller_Rishi;
 import ecomm.Globals;
 
 public class Mobile extends ProductMain{
+    
+    // Constructor for Mobile product with its appropriate data members.
     public Mobile(Globals.Category m_category,String mobile_name,String m_product_id,float m_price,int m_quantity)
     {
+        // Inheriting constuctor code from the parent class that is
+        // ProductMain class.
         super(m_category,mobile_name,m_product_id,m_price,m_quantity);
     }
 }
