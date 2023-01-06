76bf8b63dec8d07f27e65b9d83791193745c7852
Rishi0002 <92517544+Rishi0002@users.noreply.github.com>
2023-01-06 22:18:50 +0530

    Update Book.java

--- ecomm v1.1/demo/Seller_Rishi/Book.java
+++ ecomm v1.1/demo/Seller_Rishi/Book.java
@@ -3,8 +3,12 @@ package demo.Seller_Rishi;
 import ecomm.Globals;
 
 public class Book extends ProductMain {
+    
+    // Constructor for book product with its data members.
     Book(Globals.Category b_category,String b_name,String b_product_id,float b_price,int b_quantity)
     {
+        // Inheriting constructor code from parent class that is the 
+        // ProductMain class.
         super(b_category,b_name,b_product_id,b_price,b_quantity);
     }
 }
